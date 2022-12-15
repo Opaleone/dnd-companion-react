eed47b6a2da0e8bd3892448e4f298dbf5c677db9
Leone Varando <106971202+Opaleone@users.noreply.github.com>
2022-12-14 23:39:18 -0600

    Added multiple page files and navbar

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+import './styles/homepage.css'
+import './styles/characterSheet.css'
+import './styles/navbar.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ client/src/pages/characterCreation.js
@@ -0,0 +1,11 @@
+import { CharacterSheet } from "../components/characterSheet"
+import { Navbar } from "../components/navbar"
+
+export const CharacterCreation = () => {
+  return (
+    <div>
+      <Navbar />
+      <CharacterSheet />
+    </div>
+  )
+}
